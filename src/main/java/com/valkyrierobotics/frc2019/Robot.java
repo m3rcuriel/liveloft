cd09a3d69a657991f9e7951c9313ca4a6cb495a4
Lee Mracek <lee.mracek@gmail.com>
2019-07-28 00:47:58 -0700

    Initial commit

---
+++ src/main/java/com/valkyrierobotics/frc2019/Robot.java
@@ -0,0 +1,40 @@
+package com.valkyrierobotics.frc2019;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+    @Override
+    public void robotInit() {
+        // Set up the robot here!
+    }
+
+    @Override
+    public void disabledInit() {
+        // Runs when the robot enters disabled mode
+    }
+
+    @Override
+    public void autonomousInit() {
+        // Runs when the robot enters autonomous mode
+    }
+
+    @Override
+    public void teleopInit() {
+        // Runs when the robot enters teleop mode
+    }
+
+    @Override
+    public void disabledPeriodic() {
+        // Runs when the robot is in disabled mode
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        // Runs when the robot is in autonomous mode
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        // Runs when the robot is in teleop mode
+    }
+}
